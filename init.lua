da6237cbc7f4d4e177a23dfc173cf89014fa6f91
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-10-30 01:17:39 -0400

    MOAR laziness

--- init.lua
+++ init.lua
@@ -9,17 +9,22 @@ local write           = customTools.write
 local immutable       = customTools.immutable
 local immutableIpairs = customTools.immutableIpairs
 local immutablePairs  = customTools.immutablePairs
+local buildString     = customTools.buildString
 
 --! No animation because that's not implemented into Minetest
 
 local beltSpeeds = immutable({1,2,3})
 local beltAngles = immutable({0,45,-45})
 
-for _,val in immutableIpairs(beltAngles) do
+-- for _,beltSpeed in immutableIpairs(beltSpeeds) do
+for _,beltAngle in immutableIpairs(beltAngles) do
     local definition = {
         paramtype = "light",
         paramtype2 = "4dir",
 
     }
-    minetest.register_node("tech:" .. tostring(angle) .. ,definition);
+    -- minetest.register_node(, definition);
+
+    write(buildString("tech:belt_"--[[layer]], beltAngle))
 end
+-- end
